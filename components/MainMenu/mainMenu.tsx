d4b7b24250b06337a45ac5866be74a292df5d91e
Drew Krull <drew.krull@gmail.com>
2026-06-08 14:09:58 -0400

    Cleaned up logging

--- components/MainMenu/mainMenu.tsx
+++ components/MainMenu/mainMenu.tsx
@@ -40,12 +40,6 @@ export default function MainMenu() {
     setMenuOption(MAIN_MENU_OPTION);
   }
 
-  console.log("Option: " + menuOption);
-  console.log("Null User :" + !user);
-  console.log("First cond: " + (menuOption != REGISTER_OPTION));
-  console.log("Second cond: " + (menuOption != LOGIN_OPTION));
-  console.log(isOptionLockedDown);
-
   // HARD SHUT DOWN ANYONE TRYING TO LOAD THE MAIN MENU WHEN NOT LOGGED IN
   if (isOptionLockedDown) {
     return <div>INVALID MENU OPTION</div>;
